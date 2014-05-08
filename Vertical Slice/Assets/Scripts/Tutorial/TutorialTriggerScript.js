d64dbad20f567575c51ab08ea389c3be2a1e13e7
Santoska <rab_toppers.ik@hotmail.com>
2014-05-08 16:16:58 +0200

    tutorialtriggers

---
+++ Vertical Slice/Assets/Scripts/Tutorial/TutorialTriggerScript.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+private var tutorialObject:GameObject;
+private var tutorialScript:TutorialScript;
+
+function Start () {
+	tutorialObject = GameObject.Find("TutorialObject") as GameObject;
+	tutorialScript = tutorialObject.GetComponent(TutorialScript) as TutorialScript;
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter(collider:Collider) {
+	if (collider.gameObject.name == "Player") {
+		tutorialScript.nextHint();
+	}
+}
