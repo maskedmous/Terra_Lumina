faa9c25d4b5215db86407fdfd64df092da614d82
Kevin Mettes <k.mettes@home.nl>
2014-05-08 21:13:01 +0200

    onPostBuild
    Copies the directories needed  for the build to function

---
+++ Vertical Slice/Assets/Scripts/Editor/PostBuild.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+import UnityEditor.Callbacks;
+@PostProcessBuild
+static function OnPostprocessBuild( target : BuildTarget, pathToBuiltProject : String ) {
+FileUtil.CopyFileOrDirectory(Application.dataPath + "\\LevelsXML",  pathToBuiltProject.Replace(".exe", "_Data" ) + "\\LevelsXML");
+FileUtil.CopyFileOrDirectory(Application.dataPath + "\\SoundEffects",  pathToBuiltProject.Replace(".exe", "_Data" ) + "\\SoundEffects");
+FileUtil.CopyFileOrDirectory(Application.dataPath + "\\Textures",  pathToBuiltProject.Replace(".exe", "_Data" ) + "\\Textures");
+}
