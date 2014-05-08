80c9ecb84def2d157abe8c4390e10d5b716ae4fb
christiankleinh <christiank@live.nl>
2014-05-08 20:56:36 +0200

    soundEngine second iteration

---
+++ Vertical Slice/Assets/Scripts/Sound/BackgroundMusic.js
@@ -0,0 +1,47 @@
+﻿#pragma strict
+
+private var volume:float = 1.0;
+private var muteBool:boolean;
+static var bgMusicExists:boolean = false;
+
+
+function Start () {
+	
+}
+
+function Awake(){
+	DontDestroyOnLoad(this.gameObject);
+	
+	if(bgMusicExists == false){
+		this.audio.volume = volume;
+		this.audio.loop = true;
+		bgMusicExists = true;
+	}
+	else{
+		Destroy(this.gameObject);
+	}
+}
+
+function Update () {
+
+}
+
+public function changeVolume(newVolume:float){ // give a number between 0 and 1 for the volume
+	this.audio.audio.volume = newVolume;
+}
+
+public function changeMute(newMute:boolean){
+	this.audio.audio.mute = newMute;
+}
+
+public function changeLoop(newLoop:boolean){
+	this.audio.audio.loop = newLoop;
+}
+
+public function playMusic(){
+	this.audio.Play();
+}
+
+public function stopMusic(){
+	this.audio.Stop();
+}
