c62d7fa1c47e8478496a285129c3813206bafc13
christiankleinh <christiank@live.nl>
2014-05-15 17:11:24 +0200

    shroom deletion after 30 seconds

--- Vertical Slice/Assets/Scripts/Level/BumpyShroom.js
+++ Vertical Slice/Assets/Scripts/Level/BumpyShroom.js
@@ -1,5 +1,22 @@
 ﻿#pragma strict
 
+private var counter:float;
+
+function Start () {
+	counter = 30.0f;
+}
+
+function Update () {
+	if(counter >= 0.0){
+		counter -= Time.deltaTime;
+		print(counter);
+	}
+	if(counter <= 0.0){
+		Destroy(this.gameObject);
+	}
+	
+}
+
 function OnCollisionEnter(obj:Collision)
 {
 	//collision and executed once
--- Vertical Slice/Assets/Scripts/Level/NormalShroom.js
+++ Vertical Slice/Assets/Scripts/Level/NormalShroom.js
@@ -1,9 +1,18 @@
 ﻿#pragma strict
 
-function Start () {
+	private var counter:float;
 
+function Start () {
+	counter = 30.0f;
 }
 
 function Update () {
-
+	if(counter >= 0.0){
+		counter -= Time.deltaTime;
+		print(counter);
+	}
+	if(counter <= 0.0){
+		Destroy(this.gameObject);
+	}
+	
 }
