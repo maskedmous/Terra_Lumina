10c5104e999d8ba5ba23d39cf325abd644d28dee
Kevin Mettes <k.mettes@home.nl>
2014-05-18 17:14:34 +0200

    Buttons implemented
    player prefab updated for creatives
    playerinput update for button update
    texture updates

--- Vertical Slice/Assets/Scripts/Player/PlayerInputScript.js
+++ Vertical Slice/Assets/Scripts/Player/PlayerInputScript.js
@@ -1,23 +1,48 @@
 ﻿#pragma strict
 
+
+
 private var player:GameObject = null;
 private var playerController:PlayerController = null;
 
 //private var chargingJump:boolean = false;
 private var chargingShot:boolean = false;
 
-private var endLevelTriggerObject:GameObject = null;
-private var endLevelTriggerScript:LevelTrigger = null;
+private var endLevelTriggerObject:GameObject 	= null;
+private var endLevelTriggerScript:LevelTrigger 	= null;
 
-private var jumpButtonEnabled:boolean = true;
-private var normalShroomButtonEnabled:boolean = true;
-private var bumpyShroomButtonEnabled:boolean = true;
-private var movementLeftEnabled:boolean = true;
-private var movementRightEnabled:boolean = true;
+private var jumpButtonEnabled			:boolean = true;
+private var normalShroomButtonEnabled	:boolean = true;
+private var bumpyShroomButtonEnabled	:boolean = true;
+private var movementLeftEnabled			:boolean = true;
+private var movementRightEnabled		:boolean = true;
+
+//an empty guiStyle
+private var guiStyle:GUIStyle = new GUIStyle();
+//button positions
+public 	var jumpButtonTexture	:Texture = null;
+private var jumpButtonRect		:Rect;
+private var jumpButtonX			:float = 0;
+public 	var jumpButtonY			:float = 780;
+
+public 	var normalShroomButtonTexture	:Texture2D = null;
+private var normalShroomButtonRect		:Rect;
+public 	var normalShroomButtonX			:float = 1600;
+public 	var normalShroomButtonY			:float = 900;
+
+public var bumpyShroomButtonTexture	:Texture2D = null;
+private var bumpyShroomButtonRect	:Rect;
+public var bumpyShroomButtonX		:float = 1600;
+public var bumpyShroomButtonY		:float = 720;
+
+//scales for button positions
+private var originalWidth 	:float = 1920;
+private var originalHeight	:float = 1080;
+private var scale			:Vector3;
 
 public function Awake():void
 {
-	playerController = this.gameObject.GetComponent("PlayerController") as PlayerController;
+	playerController 		= this.gameObject.GetComponent("PlayerController") as PlayerController;
 }
 
 function Update ()
@@ -48,16 +73,25 @@ public function OnGUI() {
 	{
 		if (!endLevelTriggerScript.getFinished())
 		{
+			//first scale the buttons before drawing them
+			scaleButtons();
+			
+			//if the jump button is enabled it will be drawn
 			if(jumpButtonEnabled)
 			{
-				if (GUI.Button(new Rect(0, Screen.height * (5.0 / 6.0), Screen.width * (1.0 / 6.0), Screen.height * (1.0 / 6.0)), "Jump"))
+				//this is the texture of the button
+				GUI.DrawTexture(jumpButtonRect, jumpButtonTexture);
+				//this is the button itself
+				if (GUI.Button(jumpButtonRect, "", guiStyle))
 				{
 					playerController.jump();
 				}
 			}
+			
 			if(normalShroomButtonEnabled)
 			{
-				if (GUI.Button(new Rect(Screen.width * (5.0 / 6.0), Screen.height * (4.0 / 6.0), Screen.width * (1.0 / 6.0), Screen.height * (1.0 / 6.0)), "Normal Shroom"))
+				GUI.DrawTexture(normalShroomButtonRect, normalShroomButtonTexture);
+				if (GUI.Button(normalShroomButtonRect, "", guiStyle))
 				{
 					playerController.setShroom(0);
 					if (!chargingShot) chargingShot = true;
@@ -69,7 +103,8 @@ public function OnGUI() {
 			}
 			if(bumpyShroomButtonEnabled)
 			{
-				if (GUI.Button(new Rect(Screen.width * (5.0 / 6.0), Screen.height * (5.0 / 6.0), Screen.width * (1.0 / 6.0), Screen.height * (1.0 / 6.0)), "Bumpy Shroom"))
+				GUI.DrawTexture(bumpyShroomButtonRect, bumpyShroomButtonTexture);
+				if (GUI.Button(bumpyShroomButtonRect, "", guiStyle))
 				{
 					playerController.setShroom(1);
 					if (!chargingShot) chargingShot = true;
@@ -81,6 +116,30 @@ public function OnGUI() {
 			}
 		}
 	}
+}
+
+private function scaleButtons():void
+{
+	//get the current scale by using the current screen size and the original screen size
+	//original width / height is defined in a variable at top, we use an aspect ratio of 16:9 and original screen size of 1920x1080
+	scale.x = Screen.width / originalWidth;		//X scale is the current width divided by the original width
+	scale.y = Screen.height / originalHeight;	//Y scale is the current height divided by the original height
+	
+	//first put the rectangles back to its original size before scaling
+	jumpButtonRect 			= new Rect(jumpButtonX			, jumpButtonY			, jumpButtonTexture.width			, jumpButtonTexture.height);
+	normalShroomButtonRect	= new Rect(normalShroomButtonX	, normalShroomButtonY	, normalShroomButtonTexture.width	, normalShroomButtonTexture.height);
+	bumpyShroomButtonRect  	= new Rect(bumpyShroomButtonX	, bumpyShroomButtonY	, bumpyShroomButtonTexture.width	, bumpyShroomButtonTexture.height);
+	
+	//second scale the rectangles
+	jumpButtonRect 			= scaleRect(jumpButtonRect);
+	normalShroomButtonRect	= scaleRect(normalShroomButtonRect);
+	bumpyShroomButtonRect  	= scaleRect(bumpyShroomButtonRect);
+}
+
+private function scaleRect(rect:Rect):Rect
+{
+	var newRect:Rect = new Rect(rect.x * scale.x, rect.y * scale.y, rect.width * scale.x, rect.height * scale.y);
+	return newRect;
 }
 
 function readTouch()
@@ -99,12 +158,8 @@ function readTouch()
 }
 
 private function isTouchingButton():boolean
-{
+{	
 	var inputXY:Vector2 = Vector2(Input.mousePosition.x, (Input.mousePosition.y - Screen.height) * -1);
-	
-	var jumpButtonRect			:Rect 	= new Rect(0, Screen.height * (5.0 / 6.0), Screen.width * (1.0 / 6.0), Screen.height * (1.0 / 6.0));
-	var normalShroomButtonRect	:Rect 	= new Rect(Screen.width * (5.0 / 6.0), Screen.height * (4.0 / 6.0), Screen.width * (1.0 / 6.0), Screen.height * (1.0 / 6.0));
-	var bumpyShroomButtonRect	:Rect 	= new Rect(Screen.width * (5.0 / 6.0), Screen.height * (5.0 / 6.0), Screen.width * (1.0 / 6.0), Screen.height * (1.0 / 6.0));
 	
 	if(jumpButtonRect.Contains(inputXY))
 	{
@@ -121,6 +176,8 @@ private function isTouchingButton():boolean
 	
 	return false;
 }
+
+
 
 function OnMouseDown()
 {
