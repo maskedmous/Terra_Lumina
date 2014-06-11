7c5b6c7ead281f4e38b8258c4fcd25f63d4346a9
Kevin Mettes <k.mettes@home.nl>
2014-06-11 22:40:58 +0200

    public OnCollisionEnter

--- Vertical Slice/Assets/Scripts/Level/NormalShroom.js
+++ Vertical Slice/Assets/Scripts/Level/NormalShroom.js
@@ -34,7 +34,7 @@ private function grow():void {
 	this.gameObject.transform.localScale = Vector3(currentScale, currentScale, currentScale);
 }
 
-private function OnCollisionEnter(collision:Collision):void
+public function OnCollisionEnter(collision:Collision):void
 {
 	if (collision.gameObject.name == "Player") {
 		var onJumpParticle:Transform = this.gameObject.transform.FindChild("shroomJump");
